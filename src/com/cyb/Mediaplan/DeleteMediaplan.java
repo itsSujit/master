7c86baf1a72885cf9d990f8d862f65f980896a54
Sujit Karande <sujitka@cybage.com>
2017-02-24 16:03:44 +0530

    AD-21 done(Mofidy Mediaplan)

--- src/com/cyb/Mediaplan/DeleteMediaplan.java
+++ src/com/cyb/Mediaplan/DeleteMediaplan.java
@@ -3,5 +3,6 @@ package com.cyb.Mediaplan;
 public class DeleteMediaplan {
 	public static void main(String[] args) {
 		System.out.println("Deleteting Mediaplan");
+		System.out.println("Done Some Changes Later");
 	}
 }
