75923c1ff86dfda374de6805063247d0f601b649
SujitK <SujitK>
2017-03-22 23:01:21 +0530

    Added Netwok and learn ammend

---
+++ src/com/cyb/Network/AddNetwork.java
@@ -0,0 +1,8 @@
+package com.cyb.Network;
+
+public class AddNetwork {
+	public static void main(String[] args) {
+		System.out.println("Adding Network");
+		System.out.println("For ammending");
+	}
+}
